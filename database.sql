cba8d28390d2653de52b61b5165d318efbad7ce5
irfanuzair123-jpg <irfanuzair123@gmail.com>
2025-12-27 10:18:13 +0800

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE profiles (
+    student_id VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(100),
+    program VARCHAR(50),
+    email VARCHAR(100),
+    hobbies VARCHAR(200),
+    intro VARCHAR(500)
+);
